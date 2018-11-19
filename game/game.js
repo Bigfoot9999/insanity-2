d08b7a9d9a52d3e93a24ada7397242c9768a1379
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-11-18 21:42:22 -0500

    Testing JSON validation

--- game/game.js
+++ game/game.js
@@ -436,9 +436,9 @@ request.send()
 
 const saveHighScores = (title, score) => {
     let newJSON = request.response
-    newJSON[title] = score
+    //newJSON[title] = score
     console.log(newJSON)
-    request.open("POST", "https://insanity2.netlify.com/game/highscores.json", true)
+    request.open("POST", "./highscores.json", true)
     request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     request.send(JSON.stringify(newJSON))
 }
